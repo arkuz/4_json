0334885ebc5e8748f568d12662155b0d5f87554a
arkuz <arkuzka@mail.ru>
2018-08-22 23:03:04 +0300

    write funcs code

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,34 @@
 import json
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="Path of JSON file")
+args = parser.parse_args()
+filepath = os.path.abspath(args.file)
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        try:
+            json_file = json.load(file)
+            return json_file
+        except json.decoder.JSONDecodeError:
+            return None
 
 
 def pretty_print_json(data):
-    pass
+    formated_json = json.dumps(data,
+                               indent=2,
+                               ensure_ascii=False)
+    print(formated_json)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(filepath)
+    if data is None:
+        print("Load error. JSON file is incorrect.")
+        exit()
+
+    pretty_print_json(data)
