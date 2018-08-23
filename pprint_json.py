234cf39f30e1aa8db38dfe7936877ba622271a60
arkuz <arkuzka@mail.ru>
2018-08-23 21:37:17 +0300

    fixed all comments

--- pprint_json.py
+++ pprint_json.py
@@ -5,34 +5,37 @@ import sys
 
 
 def create_arg_parser():
-    return argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Path of JSON file')
+    return parser
 
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
         try:
-            file_object = json.load(file)
-            return file_object
+            decoded_json = json.load(file)
+            return decoded_json
         except json.decoder.JSONDecodeError:
             return None
 
 
-def pretty_print_json(data_object):
-    formated_json = json.dumps(data_object,
-                               indent=2,
-                               ensure_ascii=False)
+def pretty_print_json(decoded_json):
+    formated_json = json.dumps(
+        decoded_json,
+        indent=2,
+        ensure_ascii=False
+    )
     print(formated_json)
 
 
 if __name__ == '__main__':
 
     parser = create_arg_parser()
-    parser.add_argument("file", help="Path of JSON file")
     args = parser.parse_args()
     filepath = os.path.abspath(args.file)
 
-    data_object = load_data(filepath)
-    if data_object is None:
-        sys.exit("Load error. JSON file is incorrect.")
+    decoded_json = load_data(filepath)
+    if decoded_json is None:
+        sys.exit('Load error. JSON file is incorrect.')
 
-    pretty_print_json(data_object)
+    pretty_print_json(decoded_json)
